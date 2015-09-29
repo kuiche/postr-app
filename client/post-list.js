56ca092fe027aef13db31e169135ac817d571603
Samuel Trangmar-Keates <samtkeates@gmail.com>
2015-09-29 20:22:10 +0100

    Refactored

--- client/post-list.js
+++ client/post-list.js
@@ -10,3 +10,15 @@ Template.recentPosts.events({
     Meteor.call("postToFacebook", this, function(err,result) {});
   }
 });
+
+Template.recentPosts.helpers({
+  posts: function() {
+    // Find all posts and list the newest first
+    return Posts.find({}, {sort: {createdAt: -1}});
+  },
+  postHas: function (type) {
+    if (!this.services) return false;
+
+    return this.services.indexOf(type) > -1;
+  }
+});
